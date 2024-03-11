c06ac744358da115ae81619a72ba75d9a08051e6
Bright Philip <110538612+brightphi210@users.noreply.github.com>
2024-03-11 06:35:27 -0700

    a

--- todoApp/urls.py
+++ todoApp/urls.py
@@ -3,7 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('', views.create_get, name='create_get'), 
+    path('', views.home, name='home'), 
+    path('dashboard/', views.create_get, name='create_get'), 
     path('update/<str:pk>/', views.update_todo, name='update_todo'),
     path('delete/<str:pk>/', views.delete_todo, name='delete_todo'),
+    path('register/', views.signupView, name='register'),
+    path('login/', views.loginView, name='login'),
+    path('logout/', views.logoutView, name='logout'),
 ]
--- todoApp/views.py
+++ todoApp/views.py
@@ -1,7 +1,50 @@
 from django.shortcuts import render, redirect
 from .models import TodoModel
-# Create your views here.
+from .form import RegisterForm, LoginForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+# Create your vdecorators import login_requirediews here.
 
+
+def home(request):
+    return render(request, 'main.html')
+
+
+def signupView(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = RegisterForm()
+    return render(request, 'signup.html', {'form': form})
+
+
+def loginView(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+
+            if user:
+                login(request, user)
+                return redirect('/')
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form': form})
+
+
+
+def logoutView(request):
+    logout(request)
+    return redirect('/')
+
+@login_required
 def create_get(request):
     
     # ========= GET ALL DATA FROM MYTODO MODEL ======= 
@@ -14,6 +57,7 @@ def create_get(request):
         
     return render(request, 'home.html', {'todos':todos})
 
+@login_required
 def update_todo(request, pk):
     todo = TodoModel.objects.get(pk=pk)
 
@@ -23,8 +67,10 @@ def update_todo(request, pk):
         return redirect('/')
     return render(request, 'update.html', {'todo':todo})
 
-
+@login_required
 def delete_todo(request, pk):
     todo = TodoModel.objects.get(pk=pk)
     todo.delete()
     return redirect('/')
+
+
